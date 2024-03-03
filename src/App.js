6b3ffff27a19bd5ff3de3f04a426f0b00f1c31c5
Vytas <v.venckas@gmail.com>
2024-03-03 17:12:57 +0000

    initial file setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import Home from './Home';
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Home />
+            {/* Other components and content */}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <section id="home">
+            <div className="container">
+                <div className="content">
+                    <h1>Your Name</h1>
+                    <img src="path/to/your/headshot.jpg" alt="Your Name" />
+                    <p>Your brand statement goes here...</p>
+                    <p>Some indication that this is your portfolio site...</p>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Home;
