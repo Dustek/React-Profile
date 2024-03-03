74c16b3f641fbeeba434b7c4933ab07f8bac0a69
Vytas <v.venckas@gmail.com>
2024-03-03 19:26:02 +0000

    styled navbar

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './style/Header.module.css'
 
 const Header = () => {
     return (
-        <header>
-            <nav>
+        <header className={styles.header}>
+            <nav className={styles.nav}>
                 <ul>
                     <li><a href="#home">Home</a></li>
                     <li><a href="#projects">Projects</a></li>
