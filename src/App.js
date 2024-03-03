9979e3befb23141d083787ee09e150989ba14e3f
Vytas <v.venckas@gmail.com>
2024-03-03 19:07:32 +0000

    setup projects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Home from './Home';
+import ProjectGallery from './ProjectGallery'
 
 const App = () => {
     return (
         <div>
             <Header />
             <Home />
+            <ProjectGallery />
             {/* Other components and content */}
         </div>
     );
