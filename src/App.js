6726cb30f5d39132f0dcbc9b18a42486ca2e7c52
Vytas <v.venckas@gmail.com>
2024-03-03 20:59:09 +0000

    added navigation functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
+// const App = () => {
+//     return (
+//       <Router>
+//         <Header />
+//         <div>
+//           <switch>
+//             <Home />
+//             <ProjectGallery />
+//                             <Route path="/contact" component={Contact} />
+//           </switch>
+//         </div>
+//       </Router>
+//     );
+// }
+
+
+
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
 import ProjectGallery from './ProjectGallery'
-import Contact from './Contact'
+import Contact from './Contact';
 
 const App = () => {
-    return (
-        <div>
-            <Header />
-            <Home />
-            <ProjectGallery />
-            <Contact />
-            {/* Other components and content */}
-        </div>
-    );
+  return (
+      <Router>
+          <Header />
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/ProjectGallery" element={<ProjectGallery />} />
+              <Route path="/contact" element={<Contact />} />
+          </Routes>
+      </Router>
+  );
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import styles from './style/Header.module.css'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <nav className={styles.nav}>
                 <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/projects">Projects</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </nav>
         </header>
