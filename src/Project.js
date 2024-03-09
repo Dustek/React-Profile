36bac6a54099e6d47b347ebd14b6594ac834c660
Vytas <v.venckas@gmail.com>
2024-03-09 14:22:17 +0000

    added project links on hover over image

--- src/Project.js
+++ src/Project.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import schedulerImage from './images/scheduler.jpg';
 
+import './style/project.css'
+
 const Project = ({ project }) => {
+    const [isHovered, setIsHovered] = useState(false); // State to track hover state
+
+    const handleMouseEnter = () => {
+        setIsHovered(true); // Set hover state to true when mouse enters
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovered(false); // Set hover state to false when mouse leaves
+    }
+
+
     const imageMap = {
         "scheduler.jpg": schedulerImage
     };
     return (
         <div className="project-item">
             <h3>{project.title}</h3>
-            <p><a href={project.deployedLink}>Deployed Version</a></p>
-            <p><a href={project.githubLink}>GitHub Repository</a></p>
-            <img src={imageMap[project.image]} alt={project.title} />
+            <p className={`deployed-version ${isHovered ? 'show' : ''}`}>
+                <a href={project.deployedLink}>Deployed Version</a>
+            </p>
+            <p className={`github-repository ${isHovered ? 'show' : ''}`}>
+                <a href={project.githubLink}>GitHub Repository</a>
+            </p>
+            <div className="image-container" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <img src={imageMap[project.image]} alt={project.title} className="project-image" />
+            </div>
         </div>
     );
 }
