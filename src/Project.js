d471de7535b4644752e19944951af07fdc8f2a08
Vytas <v.venckas@gmail.com>
2024-03-09 13:38:25 +0000

    added working image to project component

--- src/Project.js
+++ src/Project.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import schedulerImage from './images/scheduler.jpg';
 
 const Project = ({ project }) => {
+    const imageMap = {
+        "scheduler.jpg": schedulerImage
+    };
     return (
         <div className="project-item">
             <h3>{project.title}</h3>
             <p><a href={project.deployedLink}>Deployed Version</a></p>
             <p><a href={project.githubLink}>GitHub Repository</a></p>
-            <img src={project.image} alt={project.title} />
+            <img src={imageMap[project.image]} alt={project.title} />
         </div>
     );
 }
